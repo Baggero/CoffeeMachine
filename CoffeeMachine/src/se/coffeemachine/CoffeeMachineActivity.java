1e44464ceac81762d0fe2c2198c8718a9ef43e55
Robert Bagge <robert.bagge@lunicore.se>
2012-06-29 11:33:07 +0800

    SetUp

---
+++ CoffeeMachine/src/se/coffeemachine/CoffeeMachineActivity.java
@@ -0,0 +1,13 @@
+package se.coffeemachine;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CoffeeMachineActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
